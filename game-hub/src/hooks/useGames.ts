cd05f863010385a38048fe2bd42574d055a35b15
anthonyfrantz <tonesfrantz@gmail.com>
2023-08-27 20:14:19 +1000

    Building game cards

--- game-hub/src/hooks/useGames.ts
+++ game-hub/src/hooks/useGames.ts
@@ -2,9 +2,10 @@ import { useEffect, useState } from 'react';
 import apiClient from '../services/api-client';
 import { CanceledError } from 'axios';
 
-interface Game {
+export interface Game {
     id: number;
     name: string;
+    background_image: string;
 }
 
 interface FetchGamesResponse {
@@ -25,7 +26,6 @@ const useGames = () => {
                 if (err instanceof CanceledError) return;
                 setError(err.message);
             });
-
         return () => controller.abort;
     }, []);
 
